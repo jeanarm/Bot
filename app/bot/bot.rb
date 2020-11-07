0cfe4501ea276d1bbcabb36524990464e39cba25
jeanarm <nizigiarmel2015@gmail.com>
2020-11-07 15:12:42 +0200

    change token

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -1,7 +1,7 @@
 require 'facebook/messenger'
 include Facebook::Messenger
 Facebook::Messenger::Subscriptions.subscribe(
-    access_token: access_token)
+    access_token: ENV["ACCESS-TOKEN"])
   
 
 Bot.on :message do |message|
